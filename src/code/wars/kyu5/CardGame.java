d47067fca485c5627b1612e9f74cb4d36ed96b5c
Dawid Szymczyk <dawid.szymczyk@gmail.com>
2024-08-08 10:06:02 +0200

    Rename card game

--- src/code/wars/kyu5/CardGame.java
+++ src/code/wars/kyu5/CardGame.java
@@ -6,34 +6,34 @@ public class CardGame {
     }
 
     /**
-     * Alice and Bob are playing a game. There are n cards on the table. The aim of the game is to collect the most cards. The rules:
+     * Alice and Bob are playing a game. There are numberOfCards cards on the table. The aim of the game is to collect the most cards. The rules:
      * if the number of cards is even, the players can either take half of the cards from the stack, or only 1 card -- as they choose;
      * if the number of cards is odd, the players must take 1 card.
      * Alice starts the game.
      *
      * Return the maximum number of cards Alice can collect, if Bob plays optimally (tries to get as many cards as possible for himself).
-     * @param n number of cards
+     * @param numberOfCards number of cards
      * @return maximum number of cards Alice can collect
      */
-    public static long cardGame(long n) {
+    public static long cardGame(long numberOfCards) {
         long aliceCards = 0;
         boolean aliceTurn = true;
 
-        while (n > 0) {
-            if (n % 2 == 0) {
-                if ((n / 2) % 2 == 0 && n > 4) {
-                    n--;
+        while (numberOfCards > 0) {
+            if (numberOfCards % 2 == 0) {
+                if ((numberOfCards / 2) % 2 == 0 && numberOfCards > 4) {
+                    numberOfCards--;
                     if (aliceTurn) {
                         aliceCards++;
                     }
                 } else {
-                    n /= 2;
+                    numberOfCards /= 2;
                     if (aliceTurn) {
-                        aliceCards += n;
+                        aliceCards += numberOfCards;
                     }
                 }
             } else {
-                n--;
+                numberOfCards--;
                 if (aliceTurn) {
                     aliceCards++;
                 }
