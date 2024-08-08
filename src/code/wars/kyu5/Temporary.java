66ea3eec9d6eca2c1f0256211bee1172a2d30016
Dawid Szymczyk <dawid.szymczyk@gmail.com>
2024-08-08 10:21:18 +0200

    Temporary

---
+++ src/code/wars/kyu5/Temporary.java
@@ -0,0 +1,10 @@
+package code.wars.kyu5;
+
+import static code.wars.kyu5.CardGame.cardGame;
+
+public class Temporary {
+
+    public static void main(String[] args) {
+        System.out.println(cardGame(69));
+    }
+}
